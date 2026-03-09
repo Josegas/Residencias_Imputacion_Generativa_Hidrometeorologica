687497fb011a4ecfb1a8b530898d31e72702c032
Jose Angel Garcia Perez <80491054+Josegas@users.noreply.github.com>
2026-03-09 11:55:59 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- Obtencion de Datos Crudos/download_sinaloa_raw_pro.py
+++ Obtencion de Datos Crudos/download_sinaloa_raw_pro.py
@@ -14,7 +14,7 @@ BASE_URL = (
     "{estado}/dia{clave}.txt"
 )
 
-# Cambia aquí si tu carpeta raíz cambia
+# Ruta de la carpeta Raiz
 PROJECT_ROOT = r"C:\Users\Jose Garcia\Documents\10_SEMESTRE_TEC\RESIDENCIAS\Reconstruccion_de_Base_de_Datos_Nuestro"
 
 RAW_ROOT = os.path.join(
@@ -314,3 +314,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
